27d5bf9e5ff4ca3fa1217f4db9001bd27db3b362
jzbor <jzbor@posteo.net>
2019-11-11 21:12:11 +0100

    Refactored Notifications class to a subclass of ArrayList

--- src/de/jzbor/hgvinfo/model/Notifications.java
+++ src/de/jzbor/hgvinfo/model/Notifications.java
@@ -3,28 +3,10 @@ package de.jzbor.hgvinfo.model;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Notifications implements Serializable {
-
-    private ArrayList<Notification> notificationList;
-
-    public Notifications() {
-        notificationList = new ArrayList<>();
-    }
+public class Notifications extends ArrayList<Notification> implements Serializable {
 
     public Notifications(ArrayList<Notification> notificationList) {
-        this.notificationList = notificationList;
-    }
-
-    public void add(Notification notification) {
-        notificationList.add(notification);
-    }
-
-    public Notification get(int i){
-        return notificationList.get(i);
-    }
-
-    public int length() {
-        return notificationList.size();
+        this.addAll(notificationList);
     }
 
     @Override
@@ -32,7 +14,7 @@ public class Notifications implements Serializable {
         StringBuilder string = new StringBuilder(super.toString() + ":\n");
 
         for (Notification n :
-                notificationList) {
+                this) {
             string.append("\t").append(n.getTitle()).append("\n");
         }
         return string.toString();
